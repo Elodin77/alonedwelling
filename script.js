1e1688ae9b78c3ed5a1b375b99877c8fa92d8e79
Djimon Jayasundera <1015097@student.ccgs.wa.edu.au>
2019-09-27 09:29:31 +0800

    added house and other bug fixes

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 
-
+var HOUSE_GROWTH = 1.05;
+var HOUSE_SIZE = 5; // People per house
+var WOOD_EFFICIENCY = 5; // This many people is required for 1 unit per second
+var DEATH_FACTOR = 0.8; // Affects the likeliness of death relative to the amount of people
 //FUNCTIONS
 
 
@@ -35,22 +38,34 @@ function restart(hard = 0) {
         set_cookie("woodcutter", 0);
         set_cookie("wood", 0);
         set_cookie("people", 0);
+        set_cookie("house", 1);
     }
     else {
         check_cookie("woodcutter", 0);
         check_cookie("wood", 0);
         check_cookie("people", 0);
+        check_cookie("house", 1);
     }
 }
 
+
 function add_to_cookie(key, value) {
     if (Number(get_cookie(key)) + value >= 0) {
-        if (key != "woodcutter") {
-            set_cookie(key, Number(get_cookie(key)) + value, 365);
+        if (key == "woodcutter") {
+            if (Number(get_cookie("people")) >= value) {
+                set_cookie(key, Number(get_cookie(key)) + value, 365);
+                set_cookie("people", Number(get_cookie("people")) - value, 365);
+            }
         }
-        else if (Number(get_cookie("people"))>=value){
+        else if (key == "house") {
+            var price = 100 * Math.pow(HOUSE_GROWTH, Number(get_cookie("house")));
+            if (Number(get_cookie("wood")) >= price) {
+                set_cookie(key, Number(get_cookie(key)) + value, 365);
+                set_cookie("wood", Number(get_cookie("wood")) - price);
+            }
+        }
+        else {
             set_cookie(key, Number(get_cookie(key)) + value, 365);
-            set_cookie("people", Number(get_cookie("people")) - value,365);
         }
     }
 }
@@ -58,6 +73,10 @@ function update_people() {
     // Update HTML
     document.getElementById("woodcutter").innerHTML = "Woodcutter: " + get_cookie("woodcutter");
     document.getElementById("people").innerHTML = "People: " + get_cookie("people");
+    // Check max
+    if (Number(get_cookie("house")) * HOUSE_SIZE < Number(get_cookie("people"))) {
+        set_cookie("people", Number(get_cookie("house")) * HOUSE_SIZE);
+    }
 
 
 }
@@ -65,10 +84,14 @@ function update_people() {
 function update_resources() {
     // Update Cookies
     var auto = 0.0;
-    auto += Number(get_cookie("woodcutter"))/5;
+
+    auto += Number(get_cookie("woodcutter"))/WOOD_EFFICIENCY;
     add_to_cookie("wood", auto);
-    // Update HTML
-    document.getElementById("wood").innerHTML = "Wood: " + parseInt(get_cookie("wood")) + " (" + Math.round(auto * 100) / 100+")";
+    document.getElementById("wood").innerHTML = "Wood: " + parseInt(get_cookie("wood")) + " (" + Math.round(auto * 100) / 100 + ")";
+
+    // Update BUILDS
+    document.getElementById("house").innerHTML = "House: " + parseInt(get_cookie("house"));
+    document.getElementById("house_tooltip").innerHTML = "Wood: " + parseInt(100 * Math.pow(HOUSE_GROWTH, Number(get_cookie("house"))));
 }
 function chance() {
     if (Math.floor(Math.random() * 10) == 0) {
@@ -79,11 +102,14 @@ function chance() {
         add_to_cookie("people", 4);
         notify("A family arrived at the dwelling.");
     }
+
 }
 function notify(msg) {
     return 0;
+
 }
 // NON-FUNCTIONS
 setInterval(update_people, 100);
 setInterval(update_resources, 1000);
 setInterval(chance, 1000);
+setInterval(restart, 1000);
